649c337cdc8aa59cf8022f873079a2576d9bfa26
dennis juanito <dennisjuanito@denniss-MacBook-Pro-2.local>
2018-06-27 09:49:28 -0600

    final commit

--- nesting.js
+++ nesting.js
@@ -129,8 +129,9 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
+
 
 
 
@@ -171,6 +172,11 @@ var myCar = {
 */
 
 //Code Here
+function recordCleaner() {
+  for (let i = 0; i < myCar.accidents.length; i++) {
+    myCar.accidents[i].atFaultForAccident = false;
+  }
+}
 
 
 
@@ -190,5 +196,14 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 */
 
 //Code Here
+function looper() {
+  for (let i = 0; i < numsArr.length; i++) {
+    for (let j = 0; j < numsArr[i].length; j++) {
+      numsArr[i][j] = (numsArr[i][j] % 2 === 0) ? 'even' : 'odd';
+    }
+  }
+  return numsArr;
+}
+
 
 

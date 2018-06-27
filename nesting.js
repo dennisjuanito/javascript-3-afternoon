c50483f4c2e184f3458c5cfeefc81ba1df8b0cfa
dennis juanito <dennisjuanito@denniss-MacBook-Pro-2.local>
2018-06-27 00:09:40 -0600

    minimal commit

--- nesting.js
+++ nesting.js
@@ -51,7 +51,28 @@ var employees = [
 */
 
 //Code Here
+/* var employeeUpdater = () => {
+  employees.forEach((arr, i)=> {
+    if (arr[i].firstName === "Theo") {
+      arr.splice(i, 1);
+    } else if (arr[i].firstName === "Lorie") {
+      arr[i].department = 'HR';
+    }
+  });
+  return employees;
+}; */
 
+var employeeUpdater = () => {
+
+  for (let i = 0; i < employees.length; i++) {
+    if (employees[i].firstName === "Theo") {
+      employees.splice(i, 1);
+    } else if (employees[i].firstName === "Lorie") {
+      employees[i].department = 'HR';
+    }
+  }
+  return employees;
+};
 
 
 ////////// PROBLEM 2 //////////
@@ -69,6 +90,17 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
+function removeDuplicates() {
+  for (let i = 0; i < workplaceAccidents.length; i++) {
+    for (let j = i + 1; j < workplaceAccidents.length; j++) {
+      if (workplaceAccidents[i] === workplaceAccidents[j]) {
+        workplaceAccidents.splice(j, 1);
+      }
+    }
+  }
+  return workplaceAccidents;
+}
+
 
 
 
